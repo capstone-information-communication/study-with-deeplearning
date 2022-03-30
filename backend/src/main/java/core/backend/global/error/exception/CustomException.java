8696758973cbbe718e5a8b930bbd794904804b22
chungchung <61677941+piaochung@users.noreply.github.com>
2022-03-30 23:25:32 +0900

    Feat: "추가 global exception 처리 기능 (#5)"

---
+++ backend/src/main/java/core/backend/global/error/exception/CustomException.java
@@ -0,0 +1,21 @@
+package core.backend.global.error.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public class CustomException extends RuntimeException {
+
+    private final ErrorCode errorCode = ErrorCode
+            .findByClass(this.getClass());
+
+    private HttpStatus httpStatus;
+    private String message;
+
+    public CustomException() {
+        httpStatus = errorCode.getHttpStatus();
+        message = errorCode.getMessage();
+    }
+}
---
+++ backend/src/main/java/core/backend/global/error/exception/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package core.backend.global.error.exception;
+
+import core.backend.global.error.ErrorResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(value = {CustomException.class})
+    protected ResponseEntity<ErrorResponse> handlerCustomException(CustomException e) {
+        return ErrorResponse.toResponseEntity(e.getErrorCode());
+    }
+}
