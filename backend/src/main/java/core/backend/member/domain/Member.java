1b2c0e600c6db37299334ca856a41e5603247532
chungchung <61677941+piaochung@users.noreply.github.com>
2022-03-30 22:17:51 +0900

    Feat: "추가 회원 entity 생성 (#1)"

---
+++ backend/src/main/java/core/backend/member/domain/Member.java
@@ -0,0 +1,41 @@
+package core.backend.member.domain;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String nickname;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    //-- 비즈니스 로직 --//
+    @Builder
+    public Member(String name, String nickname, String email, String password) {
+        this.name = name;
+        this.nickname = nickname;
+        this.email = email;
+        this.password = password;
+    }
+}
