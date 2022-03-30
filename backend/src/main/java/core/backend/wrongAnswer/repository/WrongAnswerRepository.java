0396e8da2042e699587e5ff6789230d013947bde
chungchung <61677941+piaochung@users.noreply.github.com>
2022-03-30 23:51:51 +0900

    Feat: "추가 오답 문제 repository 생성 (#7)"

---
+++ backend/src/main/java/core/backend/wrongAnswer/repository/WrongAnswerRepository.java
@@ -0,0 +1,9 @@
+package core.backend.wrongAnswer.repository;
+
+import core.backend.wrongAnswer.domain.WrongAnswer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WrongAnswerRepository extends JpaRepository<WrongAnswer, Long> {
+}
