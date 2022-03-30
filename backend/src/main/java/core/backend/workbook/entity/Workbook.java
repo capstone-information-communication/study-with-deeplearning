ab130015ec993a0c2a352f1719c1e68460fdee03
chungchung <61677941+piaochung@users.noreply.github.com>
2022-03-30 22:18:37 +0900

    Feat: "추가 문제집 entity 생성 (#1)"

---
+++ backend/src/main/java/core/backend/workbook/entity/Workbook.java
@@ -0,0 +1,41 @@
+package core.backend.workbook.entity;
+
+import core.backend.problem.domain.Problem;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Workbook {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false)
+    private Long memberId;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String description;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "workbook")
+    private List<Problem> problemList = new ArrayList<>();
+
+    //-- 비즈니스 로직 --//
+    @Builder
+    public Workbook(Long memberId, String title, String description, List<Problem> problemList) {
+        this.memberId = memberId;
+        this.title = title;
+        this.description = description;
+        this.problemList = problemList;
+    }
+}
