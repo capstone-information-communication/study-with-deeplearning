f9b29acb94d8e0db96baeb1c3803220dfdd09d55
chungchung <61677941+piaochung@users.noreply.github.com>
2022-03-30 23:51:03 +0900

    Feat: "추가 회원 repository 생성 (#7)"

---
+++ backend/src/main/java/core/backend/member/repository/MemberRepository.java
@@ -0,0 +1,9 @@
+package core.backend.member.repository;
+
+import core.backend.member.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
