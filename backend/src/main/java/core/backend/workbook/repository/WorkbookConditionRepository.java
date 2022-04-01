2ce3d5d7a0037d32cb3dc72b4888874a3c1b8c10
chungchung <61677941+piaochung@users.noreply.github.com>
2022-04-01 13:35:51 +0900

    Feat: "추가 workbook 동작 퀴리 구현체, 구현 (#10)"

---
+++ backend/src/main/java/core/backend/workbook/repository/WorkbookConditionRepository.java
@@ -0,0 +1,18 @@
+package core.backend.workbook.repository;
+
+import core.backend.workbook.dto.WorkbookCondition;
+import core.backend.workbook.entity.Workbook;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public interface WorkbookConditionRepository {
+    List<Workbook> findByIdOrThrow(Long id);
+
+    List<Workbook> findByTitleOrThrow(String title);
+
+    List<Workbook> findByDescriptionOrThrow(String description);
+
+    Page<Workbook> searchOrThrow(WorkbookCondition condition, Pageable pageable);
+}
