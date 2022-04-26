4295f05ba40f7c17ec2288b2e2e91ec8a365b5e6
chungchung <61677941+piaochung@users.noreply.github.com>
2022-04-27 00:12:06 +0900

    Feat: "추가 LikeWorkbook 서비스 코드 작성 (#39)"

--- backend/src/main/java/core/backend/global/error/exception/ErrorCode.java
+++ backend/src/main/java/core/backend/global/error/exception/ErrorCode.java
@@ -1,12 +1,9 @@
 package core.backend.global.error.exception;
 
 import core.backend.choice.exception.ChoiceNotFoundException;
-import core.backend.member.exception.*;
 import core.backend.commentary.exception.CommentaryNotFoundException;
-import core.backend.member.exception.ExpiredTokenException;
-import core.backend.member.exception.InvalidTokenException;
-import core.backend.member.exception.MemberNotFound;
-import core.backend.member.exception.WrongTokenException;
+import core.backend.likeWorkbook.exception.LikeWorkbookNotFoundException;
+import core.backend.member.exception.*;
 import core.backend.question.exception.QuestionNotFoundException;
 import core.backend.workbook.exception.WorkbookExistTitleException;
 import core.backend.workbook.exception.WorkbookNotAuthorException;
@@ -40,6 +37,7 @@ public enum ErrorCode {
     QUESTION_NOT_FOUND(NOT_FOUND, "문제를 찾을 수 없습니다", QuestionNotFoundException.class),
     CHOICE_NOT_FOUND(NOT_FOUND, "선택지를 찾을 수 없습니다", ChoiceNotFoundException.class),
     COMMENTARY_NOT_FOUND(NOT_FOUND, "문제 해설을 찾을 수 없습니다", CommentaryNotFoundException.class),
+    LIKE_WORKBOOK_NOT_FOUND(NOT_FOUND, "좋아요 문제집을 찾을 수 없습니다", LikeWorkbookNotFoundException.class),
     ;
 
     private final HttpStatus httpStatus;
--- backend/src/main/java/core/backend/likeWorkbook/repository/LikeWorkbookRepository.java
+++ backend/src/main/java/core/backend/likeWorkbook/repository/LikeWorkbookRepository.java
@@ -1,9 +1,11 @@
 package core.backend.likeWorkbook.repository;
 
 import core.backend.likeWorkbook.domain.LikeWorkbook;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LikeWorkbookRepository extends JpaRepository<LikeWorkbook, Long> {
+    Page<LikeWorkbook> findByMemberId(Long memberId);
 }
