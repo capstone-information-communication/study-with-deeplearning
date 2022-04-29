5ab36e6ac3dba121b531e72267d2c5dca5604ef0
chungchung <61677941+piaochung@users.noreply.github.com>
2022-04-29 13:18:13 +0900

    Feat: "수정 workbook 검색 시 defaultValue 를 설정하여 title 또는 description 이 없어도 검색이 가능, update 함수의 매개 변수 변경 (#69)"

--- backend/src/main/java/core/backend/workbook/WorkbookController.java
+++ backend/src/main/java/core/backend/workbook/WorkbookController.java
@@ -6,13 +6,11 @@ import core.backend.workbook.domain.Workbook;
 import core.backend.workbook.dto.WorkbookCondition;
 import core.backend.workbook.dto.WorkbookResponseDto;
 import core.backend.workbook.dto.WorkbookSaveRequestDto;
+import core.backend.workbook.dto.WorkbookUpdateRequestDto;
 import core.backend.workbook.exception.WorkbookExistTitleException;
 import core.backend.workbook.exception.WorkbookNotAuthorException;
-import core.backend.workbook.domain.Workbook;
-import core.backend.workbook.dto.WorkbookUpdateRequestDto;
 import core.backend.workbook.service.WorkbookService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
@@ -51,8 +49,8 @@ public class WorkbookController {
 
     @GetMapping("/workbook/search")
     public ResponseEntity<DataResponse> searchV1(
-            @RequestParam String title,
-            @RequestParam String description,
+            @RequestParam(defaultValue = "") String title,
+            @RequestParam(defaultValue = "") String description,
             @PageableDefault Pageable pageable) {
         WorkbookCondition condition = new WorkbookCondition(title, description);
         List<WorkbookResponseDto> result = workbookService.search(condition, pageable)
@@ -83,35 +81,32 @@ public class WorkbookController {
                 });
     }
 
-    @PutMapping("/workbook/{id}/{memberId}")
+    @PutMapping("/workbook/{id}")
     public ResponseEntity<WorkbookResponseDto> updateV1(
             @AuthenticationPrincipal Member member,
             @PathVariable Long id,
             @RequestBody WorkbookUpdateRequestDto dto) {
-        isAuthor(id, member.getId());
-        Long updatedId = workbookService.update(member.getId(), dto);
+        isAuthorOrThrow(id, member.getId());
+        Long updatedId = workbookService.update(id, dto);
 
         return ResponseEntity.ok(
                 WorkbookResponseDto.builder()
                         .entity(workbookService.findByIdOrThrow(updatedId)).build());
     }
 
-    private void isAuthor(Long id, Long memberId) {
+    @DeleteMapping("/workbook/{id}")
+    public HttpStatus deleteV1(
+            @AuthenticationPrincipal Member member,
+            @PathVariable Long id) {
+        isAuthorOrThrow(id, member.getId());
+        workbookService.deleteById(id);
+        return HttpStatus.OK;
+    }
+
+    private void isAuthorOrThrow(Long id, Long memberId) {
         Workbook workbook = workbookService.findByIdOrThrow(id);
         if (!workbook.getMemberId().equals(memberId)) {
             throw new WorkbookNotAuthorException();
         }
-    }
-
-    @DeleteMapping("/workbook/{id}")
-    public ResponseEntity<WorkbookResponseDto> deleteV1(
-            @AuthenticationPrincipal Member member,
-            @PathVariable Long id) {
-        isAuthor(id, member.getId());
-        Workbook deletedWorkbook = workbookService.findByIdOrThrow(id);
-        workbookService.deleteById(id);
-        return ResponseEntity.ok(
-                WorkbookResponseDto.builder()
-                        .entity(deletedWorkbook).build());
     }
 }
