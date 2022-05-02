5d60a191f84b24ab6071e76a667944d317ffa64a
chungchung <61677941+piaochung@users.noreply.github.com>
2022-05-02 12:12:35 +0900

    Feat: "추가 initDataBase likeWorkbook, wrongAnswer 데이터 (#74)"

--- backend/src/main/java/core/backend/initDataBase.java
+++ backend/src/main/java/core/backend/initDataBase.java
@@ -2,6 +2,8 @@ package core.backend;
 
 import core.backend.choice.domain.Choice;
 import core.backend.choice.domain.State;
+import core.backend.likeWorkbook.domain.LikeWorkbook;
+import core.backend.likeWorkbook.service.LikeWorkbookService;
 import core.backend.member.domain.Member;
 import core.backend.member.domain.Role;
 import core.backend.question.domain.Category;
@@ -10,6 +12,8 @@ import core.backend.question.domain.Question;
 import core.backend.question.service.QuestionService;
 import core.backend.workbook.domain.Workbook;
 import core.backend.workbook.service.WorkbookService;
+import core.backend.wrongAnswer.domain.WrongAnswer;
+import core.backend.wrongAnswer.service.WrongAnswerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
@@ -19,6 +23,8 @@ import javax.annotation.PostConstruct;
 import javax.persistence.EntityManager;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Component
 @RequiredArgsConstructor
@@ -38,12 +44,14 @@ public class initDataBase {
     static class InitService {
         private final WorkbookService workbookService;
         private final QuestionService questionService;
+        private final WrongAnswerService wrongAnswerService;
+        private final LikeWorkbookService likeWorkbookService;
 
         private final EntityManager em;
 
-        private List<Long> memberIdList = new ArrayList<>();
-        private List<Long> workbookIdList = new ArrayList<>();
-        private List<Long> questionIdList = new ArrayList<>();
+        private final List<Long> memberIdList = new ArrayList<>();
+        private final List<Long> workbookIdList = new ArrayList<>();
+        private final List<Long> questionIdList = new ArrayList<>();
 
         public void dbInit(int count) {
             for (int i = 0; i < count; i++) {
@@ -56,6 +64,9 @@ public class initDataBase {
 
                 generateChoiceBy(i);
                 generateChoiceBy(i);
+
+                generateLikeWorkbook(i);
+                generateWrongAnswer(i);
             }
         }
 
@@ -83,20 +94,19 @@ public class initDataBase {
 
         private void generateQuestionBy(int i) {
             Workbook workbook = workbookService.findByIdOrThrow(workbookIdList.get(i));
-            Category category = categoryFactory();
 
             Question question = Question.builder()
                     .workbook(workbook)
                     .commentary(Commentary.builder().comment("commentary" + i).build())
                     .title("title" + i)
                     .content("content" + i)
-                    .category(category)
+                    .category(randomCategory())
                     .build();
             em.persist(question);
             questionIdList.add(question.getId());
         }
 
-        private Category categoryFactory() {
+        private Category randomCategory() {
             switch ((int) (Math.random() * 100) % 4) {
                 case 0:
                     return Category.BLANK;
@@ -114,9 +124,53 @@ public class initDataBase {
             Question question = questionService.findByIdOrThrow(questionIdList.get(i));
             Choice choice = Choice.builder()
                     .question(question)
-                    .state(State.ANSWER)
+                    .state(randomState())
                     .content("content" + i)
                     .build();
+        }
+
+        private State randomState() {
+            if ((int) (Math.random() * 100) % 5 == 0) {
+                return State.ANSWER;
+            }
+            return State.WRONG;
+        }
+
+        private void generateWrongAnswer(int i) {
+            Workbook workbook = workbookService.findByIdOrThrow(workbookIdList.get(i));
+
+            List<Long> questionIdList = workbook.getQuestionList()
+                    .stream()
+                    .map(question -> getQuestionIdList(question, 2))
+                    .collect(Collectors.toList());
+
+            List<WrongAnswer> result = questionIdList.stream()
+                    .filter(Objects::nonNull)
+                    .map(id -> save(id, workbook.getId(), memberIdList.get(i)))
+                    .collect(Collectors.toList());
+        }
+
+        private Long getQuestionIdList(Question question, int division) {
+            return question.getId() % division == 0 ? question.getId() : null;
+        }
+
+        private WrongAnswer save(Long questionId, Long workbookId, Long memberId) {
+            WrongAnswer wrongAnswer = WrongAnswer.builder()
+                    .questionId(questionId)
+                    .workbookId(workbookId)
+                    .memberId(memberId)
+                    .build();
+            return wrongAnswerService.save(wrongAnswer);
+        }
+
+        private void generateLikeWorkbook(int i) {
+            Workbook workbook = workbookService.findByIdOrThrow(workbookIdList.get(i));
+
+            LikeWorkbook likeWorkbook = LikeWorkbook.builder()
+                    .workbookId(workbook.getId())
+                    .memberId(memberIdList.get(i))
+                    .build();
+            likeWorkbookService.save(likeWorkbook);
         }
     }
 }
