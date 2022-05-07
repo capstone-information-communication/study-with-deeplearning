553be28da7460b26c23406a716c0df1d631c0964
dkekzhs <dkekzhs5@naver.com>
2022-05-07 23:30:51 +0900

    Feat: "토큰으로 자동 로그인 기능 추가 (#97)"

--- frontend/app/src/main/java/com/smp/frontend/Activity/LoginActivity.java
+++ frontend/app/src/main/java/com/smp/frontend/Activity/LoginActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
@@ -40,12 +41,108 @@ public class LoginActivity extends AppCompatActivity {
     private  RetrofitClient retrofitClient;
 
     Toast toast;
+    private void SginInConnection(HashMap<String,Object> LoginForm){
+        retrofitClient = RetrofitClient.getInstance();
+        RestApi restApi =  RetrofitClient.getRetrofitInterface();
+        try { // 서버 종료되어있으면 catch 예외처리
+            Call<SginInResponse> signrequest =  restApi.SignIn(LoginForm);
+            signrequest.enqueue(new Callback<SginInResponse>() {
+                @RequiresApi(api = Build.VERSION_CODES.N)
+                @Override
+                public void onResponse(Call<SginInResponse> call, Response<SginInResponse> response) {
+                    //응답으로 토큰값이 와야되는데
+                    if(response.code() == 200){
+                        Object responToken =response.body().Gettoken();
+                        PreferencesManager.setToken(getApplication(),"token","Bearer "+responToken);
+                        toast.makeText(getBaseContext(), PreferencesManager.getString(getApplication(),"token"),Toast.LENGTH_LONG).show();
 
+                        Call<SginInResponse> UserInfo = restApi.GetUser(PreferencesManager.getString(getBaseContext(),"token"));
+                        UserInfo.enqueue(new Callback<SginInResponse>() {
+                            @Override
+                            public void onResponse(Call<SginInResponse> call, Response<SginInResponse> response2) {
+                                if(response2.code() == 200) {
+                                    System.out.println("response = " + response2.body().getNickname());
+                                    PreferencesManager.setData(getApplication(),"nickname",response2.body().getNickname());
+                                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                }
+                                else {
+                                    try {
+                                        JSONObject jsonObject = new JSONObject(response2.errorBody().string());
+                                        String message = jsonObject.get("message").toString();
+                                        Toast.makeText(getBaseContext(),message,Toast.LENGTH_LONG).show();
+                                    } catch (IOException|JSONException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                            }
+                            @Override
+                            public void onFailure(Call<SginInResponse> call, Throwable t) {
+                            }
+                        });
+                    }else {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response.errorBody().string());
+                            String message = jsonObject.get("message").toString();
+                            Toast.makeText(getBaseContext(),message,Toast.LENGTH_LONG).show();
+                        } catch (IOException | JSONException e) {
+                            e.printStackTrace();
+                            System.out.println("JSON 메세지 오류");
+                        }
+                    }
+
+                }
+                @Override
+                public void onFailure(Call<SginInResponse> call, Throwable t) {
+                    toast.makeText(getBaseContext(),"서버 통신에러",Toast.LENGTH_LONG).show();
+                    t.printStackTrace();
+                }
+            });
+        }catch (Exception e){
+            toast.makeText(getBaseContext(),"서버 확인",Toast.LENGTH_LONG).show();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //loginAutoTokenCheck
+        if(!(PreferencesManager.getString(getApplication(),"token").equals(null))){
+            retrofitClient = RetrofitClient.getInstance();
+            RestApi restApi =  RetrofitClient.getRetrofitInterface();
+            Call<SginInResponse> UserInfo = restApi.GetUser(PreferencesManager.getString(getBaseContext(),"token"));
+            UserInfo.enqueue(new Callback<SginInResponse>() {
+                @Override
+                public void onResponse(Call<SginInResponse> call, Response<SginInResponse> response) {
+                    if(response.code() == 200) {
+                        System.out.println("response2 = " + response.body().getEmail());
+                        PreferencesManager.setData(getApplication(),"nickname",response.body().getNickname());
+                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                    else {
+                        try {
+                            if(response.code() != 500) {
+                                JSONObject jsonObject = new JSONObject(response.errorBody().string());
+                                String message = jsonObject.get("message").toString();
+                                Toast.makeText(getBaseContext(), message, Toast.LENGTH_LONG).show();
+                            }
+                        } catch (IOException|JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+                @Override
+                public void onFailure(Call<SginInResponse> call, Throwable t) {
+                    Toast.makeText(getBaseContext(),"서버에러",Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+
 
 
         btn_login = (Button)findViewById(R.id.btn_login); //로그인 버튼
@@ -73,43 +170,7 @@ public class LoginActivity extends AppCompatActivity {
                 LoginForm.put("email", userID);
                 LoginForm.put("password", userPass);
 
-                retrofitClient = RetrofitClient.getInstance();
-                RestApi restApi =  RetrofitClient.getRetrofitInterface();
-                try { // 서버 종료되어있으면 catch 예외처리
-                    Call<SginInResponse> signrequest =  restApi.SignIn(LoginForm);
-                    signrequest.enqueue(new Callback<SginInResponse>() {
-                        @RequiresApi(api = Build.VERSION_CODES.N)
-                        @Override
-                        public void onResponse(Call<SginInResponse> call, Response<SginInResponse> response) {
-                            //응답으로 토큰값이 와야되는데
-                            if(response.code() == 200){
-                                Object responToken =response.body().Gettoken();
-                                PreferencesManager.setToken(getApplication(),"token",responToken);
-                                toast.makeText(getBaseContext(), PreferencesManager.getString(getApplication(),"token"),Toast.LENGTH_LONG).show();
-                            }else {
-                                try {
-                                    JSONObject jsonObject = new JSONObject(response.errorBody().string());
-                                    String message = jsonObject.get("message").toString();
-                                    Toast.makeText(getBaseContext(),message,Toast.LENGTH_LONG).show();
-                                } catch (IOException | JSONException e) {
-                                    e.printStackTrace();
-                                    System.out.println("JSON 메세지 오류");
-                                }
-                            }
-
-                        }
-                        @Override
-                        public void onFailure(Call<SginInResponse> call, Throwable t) {
-                            toast.makeText(getBaseContext(),"서버 통신에러",Toast.LENGTH_LONG).show();
-                            t.printStackTrace();
-                        }
-                    });
-                }catch (Exception e){
-                    toast.makeText(getBaseContext(),"서버 확인",Toast.LENGTH_LONG).show();
-                }
-          
-
-
+                SginInConnection(LoginForm);
             }
         });
     }
