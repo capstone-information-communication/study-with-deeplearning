d619ed9f205f294afc2d003a0a412e626cb194ce
chungchung <61677941+piaochung@users.noreply.github.com>
2022-05-24 16:29:48 +0900

    Feat: "추가 오답 문제집 service 코드 작성 (#128)"

---
+++ backend/src/main/java/core/backend/wrongProblem/wrongWorkbook/exception/WrongWorkbookNotFoundException.java
@@ -0,0 +1,6 @@
+package core.backend.wrongProblem.wrongWorkbook.exception;
+
+import core.backend.global.error.exception.CustomException;
+
+public class WrongWorkbookNotFoundException extends CustomException {
+}
---
+++ backend/src/main/java/core/backend/wrongProblem/wrongWorkbook/repository/WrongWorkbookRepository.java
@@ -0,0 +1,15 @@
+package core.backend.wrongProblem.wrongWorkbook.repository;
+
+import core.backend.wrongProblem.wrongWorkbook.domain.WrongWorkbook;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface WrongWorkbookRepository extends JpaRepository<WrongWorkbook, Long> {
+    List<WrongWorkbook> findByMemberId(Long memberId);
+
+}
